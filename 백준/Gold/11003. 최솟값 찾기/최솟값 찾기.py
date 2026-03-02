f68ab081de388d9a273bfc8b8fd16641f19d47ef
JongHyeon Park <67833043+JJong2003@users.noreply.github.com>
2026-03-02 22:23:12 +0900

    [Gold I] Title: 최솟값 찾기, Time: 2744 ms, Memory: 890172 KB -BaekjoonHub

--- 백준/Gold/11003. 최솟값 찾기/최솟값 찾기.py
+++ 백준/Gold/11003. 최솟값 찾기/최솟값 찾기.py
@@ -1,14 +1,23 @@
-from heapq import heappush, heappop
+from collections import deque
 
 n, l = map(int, input().split())
 A = list(map(int, input().split()))
 
-pq = [(A[0], 0)]
+dq = deque() # val, index
+ans = []
 
-ans = [A[0]]
-for i in range(1, n):
-    while pq and i-l+1 > pq[0][1]:
-        val, idx = heappop(pq)
-    heappush(pq, (A[i], i))
-    ans.append(pq[0][0])
-print(' '.join(map(str, ans)))
+for i in range(n):
+    a = A[i]
+
+    # 값이 a 보다 크다면 모두 제거
+    while dq and dq[-1][0] > a:
+        dq.pop()
+
+    # 값이 범위 l 바깥이면 제거
+    while dq and dq[0][1] < i-l+1:
+        dq.popleft()
+
+    dq.append((a, i))
+    ans.append(dq[0][0])
+    
+print(*ans)
